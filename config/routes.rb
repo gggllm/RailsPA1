7e83b3c14b37c16fb6a1d405d94987505aa7b6f7
gggllm <515815530@qq.com>
2018-09-23 20:40:38 -0700

    models scaffold set

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :subjects
   resources :courses
   resources :instructors
+  root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
