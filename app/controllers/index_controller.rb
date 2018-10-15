da8acd6432c98ee63de956b7c3b5eb24ad9d0d7f
gggllm <515815530@qq.com>
2018-10-14 22:39:19 -0700

    models scaffold set

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,12 @@
 class IndexController < ApplicationController
   def index
-    @courses=User.find(session[:user_id]).courses
+    if (!session[:user_id])
+      @courses=[]
+      return
+    end
+    @courses = User.find(session[:user_id]).courses
   end
+
   def login
 
   end
