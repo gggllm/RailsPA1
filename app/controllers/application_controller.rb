d89a4a1f60a25bc4d4394e75ef6ed96fa118e8b4
gggllm <515815530@qq.com>
2018-10-23 22:42:35 -0700

    Down case the search result.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
-
 class ApplicationController < ActionController::Base
   before_action :require_login
   include ApplicationHelper
+
   def require_login
     if !is_login(session) && !is_before_login(request)
       redirect_to :login_path
     end
-    @current_user=User.find session[:user_id]
+    if session[:user_id]
+      @current_user = User.find session[:user_id]
+    end
   end
 end
